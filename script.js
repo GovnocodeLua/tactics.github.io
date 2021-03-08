1be5e783a9cfd40fbd3143d089bf7fe0b5d8e7dc
LorgOn <37798530+GovnocodeLua@users.noreply.github.com>
2021-03-08 13:31:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
+(function() {
+  document.querySelector('input').addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) {
+      // можете делать все что угодно со значением текстового поля
+      if (document.getElementsByTagName("input")[0].value != ""){
+        const uri = 'https://www.google.ru/search?q='+document.getElementsByTagName("input")[0].value;
+        const encoded = encodeURI(uri);
+        console.log(encoded);
+        openInNewTab(encoded);
+      }
+    }
+  });
+})();
+
+/*function SendSearch(){
+  const uri = 'https://www.google.ru/search?q=шеллы';
+  const encoded = encodeURI(uri);
+  console.log(encoded);
+  alert(encoded)
+} */
